9f21110ec618e381d7228866ac229d3bffde44de
Wriqon <emperorwri@gmail.com>
2019-02-20 00:21:48 -0600

    Laid out skills pages, half of toss hands page done
    added a skills css page to help with the layout. wrote the base half of the toss hands page. updated all placeholder pages to use the footer.js

--- Wriqon/cheer/basics/hands.php
+++ Wriqon/cheer/basics/hands.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<!--  Wright Bosley, 2 Jan 2019, cheer/hands.php -->
-
+<!--  Wright Bosley, 19 Feb 2019, cheer/hands.php -->
 <head>
 	<meta charset="UTF-8">
 	<title>Cheerleading and stunting</title>
 	<link rel="stylesheet" href="../../main.css">
+	<link rel="stylesheet" href="../skillshelper.css">
 </head>
 	
 <body>
@@ -13,10 +13,62 @@
 </header>
 
 <h1>This page is under construction right now....</h1>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/kvYNXevxTxA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<article id="instructions"> <!--SKILLS START HERE!!!-->
+
+<div id="left">
+<h2>Bases:</h2>
+
+<h3>The toss</h3>
+<ol>
+	<li>Get into a good athletic stance
+	<li>Grab your fliers hips (don't squeeze too hard- a relaxed grip is good)
+	<li>Follow the flier and squat when she dips to start her jump (important to not push her down into the ground when she dips)
+	<li>WAIT as she starts to explode upwards
+	<li>Squeeze your elbows inwards to keep your arms in a power position
+	<li>Use your legs and PUSH the ground away!
+	<li>When you're nearly fully stood up start to use your arms to push your flier away
+	<li>Your legs and arms should be fully extended at the end of this step
+	<li>At the very top release your flier with a flick from your wrists! Don't stop your toss early in order to cheat and try to get to her feet quicker
+</ol>
+
+<h3>The catch</h3>
+<ol>
+	<li>Once you finish your toss and flick, bring your hands straight downwards to get underneath the feet
+	<li>Open your hands as wide as you can to get the greatest amount of space to catch with
+	<li>When your flier's feet make contact with your hands begin to squeeze them hard!
+	<li>Tighten your abs and squeeze your shoulder blades to stabilize your arms
+</ol>
+
+<h3>The pop off and set down</h3>
+<ol>
+	<li>Start the pop by dipping slightly through your legs
+	<li>Press through the ground and toss your flier up and <b>SLIGHTLY</b> forward
+	<li>Reach up and catch your flier's hips in the same way as when you started
+	<li>Slow your flier down as much as you can! A good pop off will use the same amount of effort as the toss to get up there
+	<li>Once you both have your feet back on the ground, stand back up sharp!
+</ol>
+
+</div>
+
+<div id="right">
+<h2>Fliers:</h2>
+
+<ol>
+	<li>
+	<li>
+	<li>
+</ol>
+
+</div>
+
+<iframe width="80%" height="500" src="https://www.youtube.com/embed/kvYNXevxTxA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+</article>
 
 <footer>
-	<p>Site created by WBosley. Copyright 2019. All rights reserved.</p>
 </footer></br></br></br></br></br></br></br>
+
+<script type="text/javascript" src="../../footer.js"></script>
 </body>
-</html> 
+</html>
--- Wriqon/cheer/intermediates/lowhigh.php
+++ Wriqon/cheer/intermediates/lowhigh.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<!--  Wright Bosley, 2 Jan 2019, cheer/hands.php -->
-
+<!--  Wright Bosley, 19 Feb 2019, cheer/hands.php -->
 <head>
 	<meta charset="UTF-8">
 	<title>Cheerleading and stunting</title>
 	<link rel="stylesheet" href="../../main.css">
+	<link rel="stylesheet" href="../skillshelper.css">
 </head>
 	
 <body>
@@ -14,8 +14,37 @@
 
 <h1>This page is under construction right now....</h1>
 
+<article id="instructions"> <!--SKILLS START HERE!!!-->
+
+<div id="left">
+<h2>Bases:</h2>
+
+<ol>
+	<li></li>
+	<li></li>
+	<li></li>
+</ol>
+
+</div>
+
+<div id="right">
+<h2>Fliers:</h2>
+
+<ol>
+	<li></li>
+	<li></li>
+	<li></li>
+</ol>
+
+</div>
+
+<!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/kvYNXevxTxA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+-->
+</article>
+
 <footer>
-	<p>Site created by WBosley. Copyright 2019. All rights reserved.</p>
 </footer></br></br></br></br></br></br></br>
+
+<script type="text/javascript" src="../../footer.js"></script>
 </body>
-</html> 
+</html>
